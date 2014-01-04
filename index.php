3dffd4a0bc13b2122ae75c76b200545e7794c3e4
tuna <tuna92@wru.vn>
2014-01-04 20:34:57 +0700

    first commit local

---
+++ index.php
@@ -0,0 +1,114 @@
+<?php
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Live Support by tuna92</title>
+	
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	
+	
+	<link href="styles/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<!-- <link href="styles/render.css" rel="stylesheet" type="text/css" /> -->
+	<link href="styles/fix-chat.css" rel="stylesheet" type="text/css" />
+	
+	<script src="js/jquery-1.4.2.min.js"></script>
+	
+</head>
+
+<body>
+<!-- header -->
+<div class="log" id="log" style="position: absolute; top: 0; left: 0; z-index: 10;">
+		
+</div>
+
+<header class="header">
+  <div class="container">
+    <div class="row">
+      <div class="span6 logo"> <a href="#"><img id="page_logo" src="images/livechat.jpg" width="159" height="100" alt="CSSTIME" /></a> </div>
+      <div class="span6">
+        <ul class="nav nav-pills header-nav">
+          <li class="active"><a href="index.php">Home page</a></li>
+          <li><a href="">Customer</a></li>
+          <li><a href="">Demo</a></li>
+          <li><a href="">Info</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</header>
+<!-- /header -->
+
+<div class="container" style="margin-top: 10px;"> 
+  <!-- Main hero unit for a primary marketing message or call to action -->
+  <div class="hero-unit frontpage" style="position: relative; min-height: 245px;">
+	<div class="beta" style="position: absolute; top: -4px; left: -4px;  width: 99px; height: 100px; background: url('images/beta.png')no-repeat scroll 0 0 transparent; "></div>
+    
+  </div>
+  <hr class="soften" />
+  <section id="homepage">
+    <div class="row">
+      <div class="span4">
+        <h2>Super easy to setup and customize</h2>
+        <p>No downloads, no setup fees. Just paste a line of code in your website to get a beautiful, customizable chat widget.</p>
+      </div>
+      <div class="span4">
+        <h2>Simple yet powerful features</h2>
+        <p>Starting your first chat? Or configuring Zopim to auto-target VIP customers?</p>
+      </div>
+      <div class="span4">
+        <h2>Stay connected anywhere</h2>
+        <p>Be in touch with your customers from your computer, favorite Instant Messenger or our iPhone app. </p>
+      </div>
+    </div>
+  </section>
+  <hr class="space" />
+</div>
+<!-- end container -->
+
+<footer class="footer">
+  <!--
+  <div class="container footer_content">
+    <div class="row">
+      
+    </div>
+  </div> -->
+  <!-- Contacts or something else -->
+  <div class="footer_bottom">
+    <div class="container">
+      <div class="row">
+        <div class="span6 footer_render">
+          <h3>1-800-123-1800</h3>
+          <p>Short contact information</p>
+        </div>
+        <div class="span6 frow">
+          <div class="input-prepend">
+            <h3>Stay connected</h3>
+            <p>Newsletters offering a stylish and entertaining mix of information and inspiration.</p>
+            <form class=" form-inline" />
+              <span class="add-on"><i class="icon-envelope"></i></span>
+              <input type="text" class="input-small span2" placeholder="Email" />
+              <button type="submit" class="btn"><i class="icon-check"></i>Subscribe</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /Contacts or something else --> 
+  </div>
+</footer>
+
+<div class="chatbox widget">
+	<a href="client.php" class="">
+		<img src="images/livechat.png" alt="Live Support" title="Live Support"/>
+	</a>
+</div>
+
+
+<script type="text/javascript">
+
+</script>
+
+</body>
+</html>
